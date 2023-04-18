cd19b09566863ab8a55517dbc77c7ff6a6d9fa9e
Raimonds Bortascenoks <r.bortascenoks@gmail.com>
2023-04-18 19:34:14 +0300

    Add project files.

---
+++ FluentEmployee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentInterfaceDesign
+{
+    internal class FluentEmployee
+    {
+        private Employee employee = new Employee();
+        public FluentEmployee NameOfEmployee(string fullName)
+        {
+            employee.FullName = fullName;
+            return this;
+        }
+        public FluentEmployee EmployeeDateOfBirth(string dateOfBirth)
+        {
+            employee.DateOfBirth = Convert.ToDateTime(dateOfBirth);
+            return this;
+        }
+        public FluentEmployee EmployeeDeoartment(string department)
+        {
+            employee.Department = department;
+            return this;
+        }
+        public FluentEmployee EmployeeAddress(string address)
+        {
+            employee.Address = address;
+            return this;
+        }
+
+        public void ShowDetailsOfEmployee()
+        {
+            Console.WriteLine($"Name: {employee.FullName} - {employee.Department} - {employee.DateOfBirth} - {employee.Address}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace FluentInterfaceDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] someNumbers = { 4, 3, 2, 1 };
+
+            var processedValues = someNumbers
+                .Select(n => n * 10)
+                .Where(n => n > 10);
+
+            foreach (int n in someNumbers)
+            {
+                Console.WriteLine(n * 10);
+            }
+
+            Console.WriteLine("00000000000000000000000000000");
+            ShowElements(processedValues);
+            Console.WriteLine("00000000000000000000000000000");
+
+            FluentEmployee employeeObj = new FluentEmployee();
+
+            employeeObj
+                .NameOfEmployee("Toms Berzins")
+                .EmployeeDateOfBirth("11/11/1995")
+                .EmployeeDeoartment("Vilnus")
+                .EmployeeAddress("Gedimino str. 9");
+
+            employeeObj.ShowDetailsOfEmployee();
+        }
+
+        static void ShowElements(IEnumerable<int> values)
+        {
+            foreach (var value in values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
